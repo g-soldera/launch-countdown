5b7f434fe33564bb3f6faa82f1af9827555bd2ef
Gustavo Soldera <133945929+g-soldera@users.noreply.github.com>
2025-04-27 19:19:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import "./style.css"
 import { countdownTimer } from "./timer.js"
 
-let day = 19
-let month = 8
-let year = 2023
-let hour = 10
-let minute = 0
+let day = 12
+let month = 7
+let year = 2025
+let hour = 15
+let minute = 15
 
 const timerElement = document.querySelector("#timer")
 const targetDate = new Date(year, month - 1, day, hour, minute)
